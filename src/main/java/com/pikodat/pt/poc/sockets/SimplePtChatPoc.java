a90cd80616c694ac6aef89e621fe9349326ff5ff
Veit Weber <veit@pikodat.com>
2021-07-15 14:35:34 +0200

    Initial commit

---
+++ src/main/java/com/pikodat/pt/poc/sockets/SimplePtChatPoc.java
@@ -0,0 +1,72 @@
+package com.pikodat.pt.poc.sockets;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.jboss.logging.Logger;
+
+@ServerEndpoint("/chat/{username}")
+@ApplicationScoped
+public class SimplePtChatPoc {
+
+    private static final Logger LOG = Logger.getLogger(SimplePtChatPoc.class);
+
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        sessions.put(username, session);
+        broadcast("User " + username + " joined");
+    }
+
+    @OnClose
+    public void onClose(Session session, @PathParam("username") String username) {
+        sessions.remove(username);
+        broadcast("User " + username + " left");
+    }
+
+    @OnError
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
+        sessions.remove(username);
+        LOG.error("onError", throwable);
+        broadcast("User " + username + " left on error: " + throwable);
+    }
+
+    @OnMessage
+    public void onMessage(String message, @PathParam("username") String username) {
+        if (message.startsWith("sendTo(")) {
+            String sendToUsername = message.split("\\(")[1].split("\\)")[0];
+            sendPrivateMessage(sendToUsername, message);
+
+        } else
+            broadcast(">> " + username + ": " + message);
+    }
+
+    private void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result -> {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
+        });
+    }
+
+    private void sendPrivateMessage(String user, String message) {
+        Session s = sessions.get(user);
+        s.getAsyncRemote().sendObject(message, result -> {
+            if (result.getException() != null) {
+                System.out.println("Unable to send message: " + result.getException());
+            }
+        });
+    }
+}
